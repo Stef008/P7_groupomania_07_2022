4ab92167a491478bb2a6d53d729a0608c6924239
stef008 <stephaniejoyeux8@gmail.com>
2022-08-06 00:49:14 +0200

    ajout de page login

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import loginVue from './pages/login.vue';  
+</script>
+
+<template>
+  <h1>Groupomania</h1>
+  <login-vue/>
+  
+</template>
+
+<style>
+
+
+
+</style>
---
+++ frontend/src/pages/login.vue
@@ -0,0 +1,67 @@
+<script>
+export default {
+    name: 'Login',
+}
+</script>
+
+<template>
+<main class="form-signin w-100 m-auto">
+  <form>
+    <img class="mb-4 d-block mx-auto" src="../../assets/logo-monochrome-black.png" alt="" width="80" height="80">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+  </form>
+</main>
+</template>
+
+<style>
+
+body {
+  height: 100%;
+}
+
+body {
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+</style>
