a12fd617adc42cd41a8d8ff5a9015723c2d3bdf4
stef008 <stephaniejoyeux8@gmail.com>
2022-08-06 17:15:52 +0200

    ajout de la page home

---
+++ frontend/src/pages/home.vue
@@ -0,0 +1,28 @@
+<script>
+import AddPost from "../components/addPost.vue";
+
+export default {
+    name: "HomeVue",
+    components: {
+        AddPost
+    },
+}
+</script>
+<template>
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-lg-12">
+            </div>
+        </div>
+        <AddPost/>
+    </div>  
+</template>
+
+<style scoped>
+h1 {
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: #4E5166;
+    margin-block: .5rem;
+}
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginVue from "../pages/login.vue";
-
+import HomeVue from "../pages/home.vue";
 
 const routes = [
-    { path: "/login", component: LoginVue }
+    { path: "/login", component: LoginVue },
+    { path: "/home", component: HomeVue }
 ]
 
 
