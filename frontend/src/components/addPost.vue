493248f457a014da923cd47d01aacd592c056d58
stef008 <stephaniejoyeux8@gmail.com>
2022-08-06 17:16:34 +0200

    création du component pour ajouter un post

---
+++ frontend/src/components/addPost.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+    name: "AddPost",
+} 
+</script>
+
+<template>
+    <div class="form-floating mt-3 ">
+        <input class="form-control py-3"/>
+    </div>
+    <div class="d-flex">
+        <label for="formFile" class="btn btn-secondary mt-1">Add Image</label>
+        <span class="ms-3"></span>
+        <input type="file" id="formFile" class="d-none"/>
+        <button type="button" class="btn btn-danger mt-1 ms-auto">Add post</button>
+    </div>
+
+        <hr class="dropdown-divider mt-5 mb-5"/>
+</template>
+
+<style scoped>
+.form-floating > div.form-control {  
+    height: auto;
+    min-height: 5rem;
+}
+.form-floating > div.form-control:empty:not(:focus)::before{
+    content: "Your post here";
+    color: #a0a0a0;
+}
+hr {
+    border-bottom: 1px solid #FD2D01; 
+    box-shadow: 0px 3px 13px 2px #FD2D01;
+}
+label{
+    display: grid;
+    place-content: center;
+}
+span{
+    padding: 0.6rem;
+
+}
+</style>
