b03a4ed163b161b2a8840469108a686c9f8bf29b
stef008 <stephaniejoyeux8@gmail.com>
2022-08-10 16:27:34 +0200

    ajout d'une route get pour recuperer  les posts + implementation de allposts

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const { userLogin, userSignup } = require("./controllers/user");
+const { allPosts } = require("./controllers/posts");
+const { ctrlToken } = require("./middleware/auth");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-
 app.post("/auth/login", userLogin);
 app.post("/auth/signup", userSignup);
+app.get("/posts", ctrlToken, allPosts);
+
+
 module.exports = app;
---
+++ backend/controllers/posts.js
@@ -0,0 +1,23 @@
+const post1 = {
+  user: "stef@gmail.com",
+  content: "Hello",
+  url: "http://https://picsum.photos/400/200",
+};
+const post2 = {
+  user: "theo@gmail.com",
+  content: "C'est super",
+  url: "http://https://picsum.photos/400/200",
+};
+const post3 = {
+  user: "yoyo@gmail.com",
+  content: "Whaouu",
+  url: "http://https://picsum.photos/400/200",
+};
+
+function allPosts(req, res) {
+  const posts = [post1, post2, post3];
+  const email = req.email;
+  res.send({ posts, email });
+}
+
+module.exports = { allPosts };
