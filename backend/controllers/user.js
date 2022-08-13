7a72c3ef93f1b24b60bc8a110ff4cea7fc2c44a4
stef008 <stephaniejoyeux8@gmail.com>
2022-08-13 19:21:14 +0200

    svg du 1er utilisateur dans db

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_PASSWORD;
 
-const {  prisma } = require("../dataBase/db.js");
+const { users, prisma } = require("../dataBase/db.js");
 
 async function userLogin(req, res) {
   const { email, password } = req.body;
@@ -36,26 +36,24 @@ function checkPassword(user, password) {
 
 async function userSignup(req, res) {
   const { email, password, ctrlPassword } = req.body;
-  try {
-
-    if (password !== ctrlPassword) return res.status(400).send({ error: "Incorrect password" });
-    const registeredUser = await getUser(email);
-    if (registeredUser != null) return res.status(400).send("User already registered");
-    
-    const hash = awaitpasswordHash(password)
-    const user = await registerUser ({ email, password: hash })
-    res.send({ user })
-  }
-  catch(error) { res.status(500).send({ error })
-  }
+  if (password !== ctrlPassword)
+    return res.status(400).send({ error: "Incorrect password" });
+  const user = getUser(email);
+  if (user != null) return res.status(400).send("User already registered");
+  passwordHash(password)
+    .then((hash) => {
+      registerUser({ email, password: hash });
+      res.send({ email: email });
+    })
+    .catch((error) => res.status(500).send({ error }));
 }
 
 passwordHash = (password) => {
   return bcrypt.hash(password, 10);
 };
 
-registerUser = (user) => {
-  return prisma.user.create({ data: user })
+userSaved = (user) => {
+  users.push(user);
 };
 
 module.exports = { userLogin, userSignup };
